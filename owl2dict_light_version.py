a93dd41dfd6191050346dd9b7bb74f2605dcee03
DEBBIE- database of biomedical materials <56260193+ProjectDebbie@users.noreply.github.com>
2020-03-24 10:36:07 +0100

    Update owl2dict_light_version.py

--- owl2dict_light_version.py
+++ owl2dict_light_version.py
@@ -3,9 +3,9 @@
 import owlready2
 
 #input to provide
-ontology = 'path/to/extracted/ontology.owl'
-wanted_superclasses = ['blabla', 'blabla2']
-new_dictionary = 'dictionary_name.txt'
+ontology = '/local/location/of/ontology/For_annotations/Thesaurus.owl'
+wanted_classes_and_new_labels = {'Abnormal Cell' : 'Cell', 'Tissue Culture' : 'ResearchTechnique' , 'Cellular Process': 'AssociatedBiologicalProcess'}
+new_dictionary = 'name.txt'
 
 
 # load ontology
@@ -17,6 +17,7 @@ namespace = onto.get_namespace(ontology)
 class_list = list(onto.classes())
 label_list = []
 ancestor_list = []
+relevant_terms = []
 
 #get all classes (if needed)
 for c in class_list:
@@ -25,9 +26,15 @@ for c in class_list:
 #get all classes from wanted ancestors
 for c in class_list:
     ancestors = list(c.ancestors())
+    print(ancestors)
     for a in ancestors:
-        for superclass in wanted_superclasses:
-            x = a.label
-            if superclass in str(x):
+        x = a.label
+        for term, label in wanted_classes_and_new_labels.items():
+            if term in str(x) and c not in relevant_terms:
+                relevant_terms.append(c)
                 with open(new_dictionary, 'a') as n:
-                    n.write(c.label[0] + '\t' + 'LABEL=' + x[0] +'\n')
+                    n.write(c.label[0] + '\t' + 'LABEL=' + label + '\n')
+
+
+
+

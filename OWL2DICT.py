244d103f039253c9c1c7d3c7244707e05dbb5cd0
mckitrick <austin.mckitrick01@estudiant.upf.edu>
2020-08-13 18:18:06 -0400

    owltodict updated

--- OWL2DICT.py
+++ OWL2DICT.py
@@ -7,9 +7,13 @@ import sys
 import owlready2
 from owlready2 import get_ontology
 import re
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 # get filename
-path, file = os.path.split("/location/of/DEB_ontology.owl")
+# path, file = os.path.split("DEB_20200220.owl")
+path, file = os.path.split("DEB_ontology_13082020.owl")
 # remove "owl" from file name, keep "."
 file_name = file[:-3]
 # load ontology
@@ -19,6 +23,7 @@ removal = ["[", file_name, "]", " ", '.value(True)', '.some(True)', '.some(None)
 with open("DEB_ONTOLOGY.lst", 'w') as f:
     # create dictionaries for synonyms, all classes of individual terms, alternative lables (altLab) and properties
     synonyms = {}
+    ids = {}
     altLabs = {}
     ontology_path = {}
     properties = {}
@@ -35,32 +40,46 @@ with open("DEB_ONTOLOGY.lst", 'w') as f:
                        "ManufacturedObjectFeatures", "MaterialProcessing", "StudyType", "CellType"]
     ont_classes = onto.classes()
     for x in ont_classes:
-        id = x.name
+        id = x
+        terms = x.name
         altLab = re.sub(r'|'.join(map(re.escape, removal)), '', str(x.altLabel))
+        altLab = altLab.split(',')
+        altLab = list(filter(None, altLab))
+        altLab = [i.replace("'", "") for i in altLab]
+        # print(altLab)
         synonym = re.sub(r'|'.join(map(re.escape, removal)), '', str(x.equivalent_to))
-        synonyms[id] = synonym
-        altLabs[id] = altLab
+        # print(synonym)
+        ids[terms] = id
+        altLabs[terms] = altLab
+        synonyms[terms] = synonym
         classes = [i for i in x.is_a]
+        # print(classes)
         class_list = list(classes)
-        properties[id] = ''
+        properties[terms] = ''
         # if term is in superclass_list, term is superlclass (i.e. Biomaterial = Biomaterial)
         if len(class_list) == 1:
-            if id in superclass_list:
-                dict_all_terms[id] = id
+            if terms in superclass_list:
+                dict_all_terms[terms] = terms
             else:
                 for term in removal:
                     c = str(class_list)
                     d = re.sub(r'|'.join(map(re.escape, removal)), '', c)
-                    dict_all_terms[id] = d
+                    dict_all_terms[terms] = d
         # if term is not in super class list
         # # #if length is more than 1, owl.Thing is [0]
         else:
             a = str(class_list[1:2]).split("&")
             b = [re.sub(r'|'.join(map(re.escape, removal)), '', a[0])]
             onProperty = [re.sub(r'|'.join(map(re.escape, removal)), '', a[1])]
-            dict_all_terms[id] = b[0]
-            properties[id] = onProperty[0]
-    
+            dict_all_terms[terms] = b[0]
+            properties[terms] = onProperty[0]
+        # f.write(str(terms) + '\t' + 'LABEL=' + '\t' + 'ID=' + str(id) + '\t' +  '\n')
+        # if len(altLab) == 0:
+        #     pass
+        # else:
+        #     for i in altLab:
+        #         f.write(str(i) + '\t' + 'LABEL=' + '\t' + 'ID=' + str(id) + '\t' + 'PREFSYNONYM=' + str(terms) +  '\n')
+
     # we have all terms and their direct parent class
     # now we retrieve their superparent class
     for k, v in dict_all_terms.items():
@@ -73,47 +92,62 @@ with open("DEB_ONTOLOGY.lst", 'w') as f:
         else:
             variable = dict_all_terms.get(parent)
             ontology_path[k].append(variable)
-            while variable not in superclass_list:
+            if variable not in superclass_list:
                 variable = dict_all_terms.get(variable)
                 ontology_path[k].append(variable)
             else:
                 dict_superclasses[term] = variable
-   
-
-
-    #clean alternative labels
-    altLabs = {k: v.replace("'", "") for k, v in altLabs.items()}
+    # pp.pprint(dict_superclasses)
+    # pp.pprint(altLabs)
+    # # print(altLabs)
 
     # merge synonym, ontology_path, and dict3 dictionaries into NEW dictionary
-    merged.update({key: [value1, value2, value3, value4, value5] for key, value1, value2, value3, value4, value5 in
-                   zip(dict_superclasses.keys(), dict_superclasses.values(), ontology_path.values(), synonyms.values(),
-                       properties.values(), altLabs.values())})
+    # pp.pprint(dict_superclasses)
+    # pp.pprint(altLabs)
+    # pp.pprint(ids)
+    keys = dict_superclasses.keys()
+    ids_sorted = {i:ids[i] for i in dict_superclasses.keys()}
+    synonyms_sorted = {i:synonyms[i] for i in dict_superclasses.keys()}
+    properties_sorted = {i:properties[i] for i in dict_superclasses.keys()}
+    altLabs_sorted = {i:altLabs[i] for i in dict_superclasses.keys()}
+    ontology_path_sorted = {i:ontology_path[i] for i in dict_superclasses.keys()}
+    values = zip(dict_superclasses.values(), ids_sorted.values(), ontology_path_sorted.values(), synonyms_sorted.values(), properties_sorted.values(), altLabs_sorted.values())
+    merged = dict(zip(keys, values))
+    # pp.pprint(merged)
+    # merged.update({key: [value1, value2, value3, value4, value5] for key, value1, value2, value3, value4, value5 in
+    #                zip(dict_superclasses.keys(), dict_superclasses.values(), ids.values(), synonyms.values(),
+    #                    properties.values(), altLabs.values())})
+    # print(merged)
 
-    # if key has a property or altLabel associated, a new line with property as key is created
-    for k, v in list(merged.items()):
-        if len(v[3]) is not 0:
-            merged.update({v[3]: [v[0], v[1], v[2], v[4], k]})
-        elif len(v[4]) is not 0:
-            merged.update({v[4]: [v[0], v[1], v[2], v[3], k]})
+    # # if key has a property or altLabel associated, a new line with property as key is created
+    # for k, v in list(merged.items()):
+    #     if len(v[3]) is not 0:
+    #         merged.update({v[3]: [v[0], v[1], v[2], v[4], k]})
+    #     elif len(v[4]) is not 0:
+    #         merged.update({v[4]: [v[0], v[1], v[2], v[3], k]})
     
     # clean all terms
     for k, v in merged.items():
-        synonym = v[2]
-        property = v[3]
-        altLab = v[4]
+        path = v[2]
+        path = [x for x in path if x is not None]
+        path = [ x for x in path if "owl.Thing" not in x ]
+        synonym = v[3]
+        property = v[4]
+        altLab = v[5]
         if k.isupper() == True:
             k = k
         elif synonym.isupper() == True:
             synonym = synonym
         elif property.isupper() == True:
             property = property
-        elif altLab.isupper() == True:
-            altLab = altLab
+        # elif altLab.isupper() == True:
+        #     altLab = altLab
+        #     print(altLab)
         else:
             clean_k = re.sub(r"([A-Z])", r" \1", k).split()
             clean_synonym = re.sub(r"([A-Z])", r" \1", synonym).split()
             clean_property = re.sub(r"([A-Z])", r" \1", property).split()
-            clean_altLab = re.sub(r"([A-Z])", r" \1", altLab).split()
+            # clean_altLab = altLab.split(",")
             k = ' '.join(clean_k)
             k = k.replace("3 D", "3D")
             k = k.replace("- ", "-")
@@ -122,15 +156,25 @@ with open("DEB_ONTOLOGY.lst", 'w') as f:
             synonym = synonym.replace("3 D", "3D")
             synonym = synonym.replace("- ", "-")
             synonym = synonym.lower()
-            altLab = ' '.join(clean_altLab)
-            altLab = altLab.replace("3 D", "3D")
-            altLab = altLab.replace("- ", "-")
-            altLab = altLab.lower()
             property = ' '.join(clean_property)
             property = property.replace("3 D", "3D")
             property = property.replace("- ", "-")
             property = property.lower()
-        f.write(k + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(v[1]) + '\t' + 'SYNONYM=' + str(
-            synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'ALTLABEL=' + str(altLab) +  '\n')
+        f.write(str(k) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(
+            synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + '\n')
+        for i in altLab:
+            if i.isupper() == True:
+                f.write(str(i) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'PREFSYN=' + str(k) +  '\n')
+            elif i == 'rhBMP':
+                f.write(str(i) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'PREFSYN=' + str(k) +  '\n')
+            elif '-' in i:
+                f.write(str(i) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'PREFSYN=' + str(k) +  '\n')
+            elif '(' in i:
+                f.write(str(i) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'PREFSYN=' + str(k) +  '\n')
+            else:
+                a = re.sub(r"([A-Z])", r" \1", i).split()
+                a = ' '.join(a)
+                a = a.lower()
+                f.write(str(a) + '\t' + 'LABEL=' + str(v[0]) + '\t' + 'PATH=' + str(path) + '\t' + 'SYNONYM=' + str(synonym) + '\t' + 'PROPERTY=' + str(property) + '\t' + 'PREFSYN=' + str(k) +  '\n')
 
 
